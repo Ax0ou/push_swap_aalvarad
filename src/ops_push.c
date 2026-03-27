354f22d5022a362a0e6f5822940c663585030a31
Axel Alvarade <axel.alvarade@hotmail.fr>
2026-03-27 14:32:36 +0100

    sorting

--- src/ops_push.c
+++ src/ops_push.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ops_push.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aalvarad <aalvarad.studient.42lausanne.    +#+  +:+       +#+        */
+/*   By: aalvard <aalvarad@student.42lausanne.ch    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 15:38:25 by aalvarad          #+#    #+#             */
-/*   Updated: 2026/03/25 13:38:58 by aalvarad         ###   ########.fr       */
+/*   Updated: 2026/03/27 13:14:06 by aalvard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 void	pa(t_node **stack_a, t_node **stack_b)
 {
 	t_node *tmp;
-	
+
 	if (!(*stack_b))
 		return;
-	tmp = (*stack_a);
-	(*stack_a) = (*stack_b)->next;
+	tmp = (*stack_b);
+	(*stack_b) = (*stack_b)->next;
 	tmp->next = NULL;
 	stack_add_front(stack_a, tmp);
-	write(1, "pa\n", 3);	
+	write(1, "pa\n", 3);
 }
 
 void	pb(t_node **stack_b, t_node **stack_a)
 {
 	t_node *tmp;
-	
+
 	if (!(*stack_a))
 		return;
 	tmp = (*stack_a);
---
+++ src/small_sort.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   small_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalvard <aalvarad@student.42lausanne.ch    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/27 11:18:31 by aalvard           #+#    #+#             */
+/*   Updated: 2026/03/27 14:30:37 by aalvard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_two(t_node **stack_a)
+{
+	if ((*stack_a)->value > (*stack_a)->next->value)
+		sa(stack_a);
+}
+
+void	sort_three(t_node **stack_a)
+{
+	int a;
+	int b;
+	int c;
+
+	a = (*stack_a)->value;
+	b = (*stack_a)->next->value;
+	c = (*stack_a)->next->next->value;
+	if (a < b && b < c)
+	    return;
+	if (a > b && b > c)
+	{
+		sa(stack_a);
+		rra(stack_a);
+	}
+	if (a > b && b < c && a > c)
+	    ra(stack_a);
+	if (a > b && b < c && a < c)
+	    sa(stack_a);
+	if (a < b && b > c && a > c)
+	    rra(stack_a);
+	if (a < b && b > c && a < c)
+	{
+		sa(stack_a);
+		ra(stack_a);
+	}
+}
+
+void    sort_five(t_node **stack_a, t_node **stack_b)
+{
+    while (find_min_pos(*stack_a) != 0)
+        ra(stack_a);
+    pb(stack_b, stack_a);  // min → B
+	while (find_min_pos(*stack_a) != 0)
+        ra(stack_a);
+    pb(stack_b, stack_a);
+	sort_three(stack_a);
+    pa(stack_a, stack_b);
+    pa(stack_a, stack_b);
+}
+
+int find_min_pos(t_node *stack_a)
+{
+    t_node  *current;
+    int     min;
+    int     pos;
+    int     i;
+
+	if (!stack_a)
+		return(-1);
+    current = stack_a;
+    min = stack_a->value;
+    pos = 0;
+    i = 0;
+
+    while (current)
+    {
+        if (current->value < min)
+        {
+            min = current->value;
+            pos = i;
+        }
+        current = current->next;
+        i++;
+    }
+    return (pos);
+}
+
+int is_sorted(t_node *stack_a)
+{
+	if (!stack_a)
+		return(0);
+	while (stack_a->next)
+	{
+    	if (stack_a->value > stack_a->next->value)
+        	return (0);
+    	stack_a = stack_a->next;
+	}
+	return (1);
+}
